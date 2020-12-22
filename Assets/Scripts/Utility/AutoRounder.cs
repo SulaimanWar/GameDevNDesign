957a4440e6717a3d3bad04dcd1040769cc174138
SulaimanWar <sulaimanwar18@gmail.com>
2020-12-23 00:31:59 +0800

    -Auto rounder update

--- Assets/Scripts/Utility/AutoRounder.cs
+++ Assets/Scripts/Utility/AutoRounder.cs
@@ -1,18 +1,88 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEditor;
 
-public class AutoRounder : MonoBehaviour
+public class AutoRounder : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Utility/Auto Rounder")]
+    static void Init()
     {
-        
+        var window = GetWindow<AutoRounder>();
+        window.Show();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        GUILayout.Label("AUTO ROUNDER");
+
+        Separator(2);
+
+        GUILayout.Label("Round to:");
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Nearest"))
+        {
+            Round("Nearest");
+        }
+
+        if (GUILayout.Button("Highest"))
+        {
+            Round("Highest");
+        }
+
+        if (GUILayout.Button("Lowest"))
+        {
+            Round("Lowest");
+        }
+
+        GUILayout.EndHorizontal();
+
+        Separator(2);
+
+        GUILayout.Label("INSTRUCTION");
+        GUILayout.Label("1) Select all the sprites that you want to round its position");
+        GUILayout.Label("2) Click on the button that best describes how you want the position to be rounded to");
     }
+
+
+    void Round(string roundType)
+    {
+        GameObject[] selectedGOs = Selection.gameObjects;
+
+        foreach(GameObject curGO in selectedGOs)
+        {
+            float targetPosX = curGO.transform.position.x;
+            float targetPosY = curGO.transform.position.y;
+
+            switch (roundType)
+            {
+                case "Nearest":
+                    targetPosX = Mathf.Round(targetPosX);
+                    targetPosY = Mathf.Round(targetPosY);
+                    break;
+
+                case "Highest":
+                    targetPosX = Mathf.Ceil(targetPosX);
+                    targetPosY = Mathf.Ceil(targetPosY);
+                    break;
+
+                case "Lowest":
+                    targetPosX = Mathf.Floor(targetPosX);
+                    targetPosY = Mathf.Floor(targetPosY);
+                    break;
+            }
+
+            curGO.transform.position = new Vector3(targetPosX, targetPosY, curGO.transform.position.z);
+        }
+    }
+
+
+    void Separator(int height)
+    {
+        for(int i=0; i<height; i++)
+        {
+            EditorGUILayout.Separator();
+        }
+    }
+
 }
