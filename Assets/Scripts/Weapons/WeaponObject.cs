57a0c2c9c34545cc79adefad475264980b48a097
SulaimanWar <sulaimanwar18@gmail.com>
2020-12-23 22:55:05 +0800

    -Created reticle system

--- Assets/Scripts/Weapons/WeaponObject.cs
+++ Assets/Scripts/Weapons/WeaponObject.cs
@@ -14,8 +14,34 @@ public class WeaponObject : MonoBehaviour
     KeyCode weaponCKeycode = KeyCode.Alpha3;
     KeyCode weaponDKeycode = KeyCode.Alpha4;
 
+    enum Direction
+    {
+        LEFT, UP, RIGHT, DOWN
+    }
+    Direction dirEnum;
+    Transform playerTransform;
+
+    private Camera mainCamera;
+    private GameObject reticle;
+
+    private Vector3 direction;
+    private Vector3 mousePosition;
+    private Vector3 reticlePosition;
+    public GameObject reticlePrefab;
+
+
+    private void Start()
+    {
+        playerTransform = transform;
+
+        Cursor.visible = false;
+        mainCamera = Camera.main;
+        reticle = Instantiate(reticlePrefab);
+    }
+
     private void Update()
     {
+        #region INPUT
         if (Input.GetKeyDown(weaponAKeycode))
         {
             SetData(0);
@@ -35,6 +61,14 @@ public class WeaponObject : MonoBehaviour
         {
             SetData(3);
         }
+        #endregion
+
+        #region RAYCAST
+
+        GetMousePosition();
+        MoveReticle();
+
+        #endregion
     }
 
     void SetData(int weaponIndex)
@@ -47,4 +81,24 @@ public class WeaponObject : MonoBehaviour
 
         spriteRenderer.sprite = curWeaponData.weaponSprite;
     }
+
+
+    private void GetMousePosition()
+    {
+        // Get Mouse Position
+        mousePosition = Input.mousePosition;
+        mousePosition.z = 5f;  // We set this value to ensure the camera always stays infront to view everything in game
+
+        // Get World space position
+        direction = mainCamera.ScreenToWorldPoint(mousePosition);
+        direction.z = transform.position.z;
+        reticlePosition = direction;
+    }
+    private void MoveReticle()
+    {
+        reticle.transform.rotation = Quaternion.identity; //set the normal rotation
+        reticle.transform.position = reticlePosition;
+    }
+
+
 }
