c42e4e37baaaf6ee6d85d27a73c6e6d540854b38
SulaimanWar <sulaimanwar18@gmail.com>
2020-12-25 10:52:55 +0800

    Added pickup system

---
+++ Assets/Scripts/Pickup/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public enum PickupType
+    {
+        WEAPON, INVENTORY
+    }
+    public PickupType pickupType;
+
+    public enum KeyType
+    {
+        FLOAT, INT, STRING
+    }
+
+    [Space(20)]
+    
+    public KeyType keyType;
+
+    public string keyName;
+
+    public float floatKey;
+    public int intKey;
+    public string stringKey;
+}
---
+++ Assets/Scripts/Pickup/PickupSystem.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+public class PickupSystem : MonoBehaviour
+{
+    enum Direction
+    {
+        LEFT, UP, RIGHT, DOWN
+    }
+    Direction dirEnum;
+    Transform playerTransform;
+
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h > 0.5f)
+        {
+            dirEnum = Direction.RIGHT;
+        }
+        else if (h < -0.5f)
+        {
+            dirEnum = Direction.LEFT;
+        }
+
+        if (v > 0.5f)
+        {
+            dirEnum = Direction.UP;
+        }
+        else if (v < -0.5f)
+        {
+            dirEnum = Direction.DOWN;
+        }
+
+        Vector2 rayDir = GetDirection();
+
+        RaycastHit2D rayHit = Physics2D.Raycast(playerTransform.position, rayDir * 2f);
+        if (rayHit.collider == null)
+        {
+            return;
+        }
+        if (rayHit.collider.tag == "Pickup")
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Pickup pickup = rayHit.collider.GetComponent<Pickup>();
+
+                switch (pickup.pickupType)
+                {
+                    case Pickup.PickupType.INVENTORY:
+                        SetKey(pickup);
+                        break;
+                }
+            }
+        }
+    }
+
+    Vector2 GetDirection()
+    {
+        Vector2 rayDir = new Vector2();
+
+        switch (dirEnum)
+        {
+            case Direction.LEFT:
+                rayDir = -transform.right;
+                break;
+
+            case Direction.UP:
+                rayDir = transform.up;
+                break;
+
+            case Direction.RIGHT:
+                rayDir = transform.right;
+                break;
+
+            case Direction.DOWN:
+                rayDir = -transform.up;
+                break;
+        }
+
+        return rayDir;
+    }
+
+    void SetKey(Pickup pickup)
+    {
+        switch (pickup.keyType)
+        {
+            case Pickup.KeyType.FLOAT:
+                PlayerPrefs.SetFloat(pickup.keyName, pickup.floatKey);
+                break;
+
+            case Pickup.KeyType.INT:
+                PlayerPrefs.SetInt(pickup.keyName, pickup.intKey);
+                break;
+
+            case Pickup.KeyType.STRING:
+                PlayerPrefs.SetString(pickup.keyName, pickup.stringKey);
+                break;
+        }
+    }
+}
