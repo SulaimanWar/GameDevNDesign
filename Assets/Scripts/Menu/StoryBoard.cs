4f640e934f49c54243c5d41a594dce1452353b80
Z_Landia <1025815213@qq.com>
2020-12-29 02:01:28 +0800

    Here are things not imolemented or need fixture: 1. enemy, BOSS (path, attack); 2. kill the boss to get key information; 3. weapon 4. chest for HP and shield

---
+++ Assets/Scripts/Menu/StoryBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryBoard : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GamePaused = false;
+    }
+}
---
+++ Assets/Scripts/Menu/end.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class end: MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void QuitToHome()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GamePaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
